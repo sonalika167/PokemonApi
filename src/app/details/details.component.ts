4f2358f075ce53e10307b0b6492af03b39f849e8
Sonalika Porwal <sonalikaporwal167@gmail.com>
2020-05-13 13:08:22 +0530

    Notepad added in the details page.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -14,7 +14,10 @@ export class DetailsComponent implements OnInit {
   pokeData = {};
   pokemonName = '';
   detailurl = 'https://pokeapi.co/api/v2/pokemon/';
-
+  notes:any;
+  textArea = '';
+  localStorageNotes = '';
+  
   ngOnInit(): void {
     this.pokemonName = this.activatedRoute.snapshot.paramMap.get("pokemonname");
     console.log(this.pokemonName);
@@ -23,6 +26,23 @@ export class DetailsComponent implements OnInit {
       this.pokeData = res;
     })
     console.log(this.auth.detailurl);
+    
+    
+    // get the stored notes from localStorage and put it inside textarea
+    this.localStorageNotes = localStorage.getItem(this.pokemonName);
+    if(this.localStorageNotes){
+      this.textArea = this.localStorageNotes;
+    }
+
+  }
+
+  updateNotes(e){
+    console.log(e.value);
+    this.textArea = e.value;
+
+    // save the textarea notes to localstorage
+    localStorage.setItem(this.pokemonName, this.textArea);
+    alert("Saved Your notes!!")
   }
 
 }
