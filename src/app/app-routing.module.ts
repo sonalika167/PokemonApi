7217996126537d5a703f916c8c8491505502efc7
Sonalika Porwal <sonalikaporwal167@gmail.com>
2020-05-13 12:19:48 +0530

    Fixed some issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { DetailsComponent } from './details/details.component'
 
 const routes: Routes = [
   { path: '', component: ListComponent},
-  {path: 'details', component: DetailsComponent },
+  {path: 'details/:pokemonname', component: DetailsComponent },
   
 ];
 
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -8,15 +9,20 @@ import { AuthService } from '../auth.service';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService,public activatedRoute: ActivatedRoute) { }
 
-pokeData = {};
+  pokeData = {};
+  pokemonName = '';
+  detailurl = 'https://pokeapi.co/api/v2/pokemon/';
 
   ngOnInit(): void {
-    this.auth.showPokemonDetails(this.auth.detailurl).subscribe((res)=>{
+    this.pokemonName = this.activatedRoute.snapshot.paramMap.get("pokemonname");
+    console.log(this.pokemonName);
+    this.auth.showPokemonDetails(this.detailurl + this.pokemonName).subscribe((res)=>{
       console.log(res);
       this.pokeData = res;
     })
+    console.log(this.auth.detailurl);
   }
 
 }
